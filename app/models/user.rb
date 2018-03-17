44b76805827466fa5906a4775f065819974337bd
luuufan <angelia.fan@gmail.com>
2018-03-16 17:13:59 -0700

    finish user table & model

---
+++ app/models/user.rb
@@ -0,0 +1,36 @@
+class User < ApplicationRecord
+  attr_reader :password
+  validates :username, :password_digest, :session_token, :email, presence: true
+  validates :username, :eamil, :session_token, uniqueness: true
+  validates :password, length: { in: 6..20, allow_nil: true }
+  before_validation :ensure_session_token
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, format: { with: VALID_EMAIL_REGEX, message: "Invalid, example: 'demo@sungrass.com' "}
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    user && user.is_password?(password) ? user :nil
+  end
+
+  def reset_session_token
+    self.session_token = SecureRandom.urlsafe_base64
+    self.save
+    self.session_token
+  end
+
+  private
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64
+  end
+
+end
